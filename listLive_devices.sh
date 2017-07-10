7f3ea55f16c2c746831c64da4fab17fee8f6725c
Istyak Rana <istyak370@gmail.com>
2017-07-10 11:17:18 +0600

    Update listLive_devices.sh
    using nmap

--- listLive_devices.sh
+++ listLive_devices.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 fping -c1 -t500 $(for i in `seq # #`; do echo #.#.#.$i;done )  2> /dev/null |  awk ' { print $1 } '
 
+nmap -sP #.#.#.* | awk ' NR % 2 == 1 { print $5} '
+
 # will show only the alive devices on given seq
 # used in to find alive devices form dhcp pool # to #
